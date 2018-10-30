f1bc07035f73d0d1b8526987b3bbbb6102fc9912
Melanie Villela <melanienvillela@gmail.com>
2018-10-30 07:05:32 -0500

    WIP
    Updated restaurants object, added markers.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 import Sidebar from './Components/Sidebar';
 import NeighborhoodMap from './Components/NeighborhoodMap';
 
+const restaurants = require('./Components/restaurants');
+
 class App extends Component {
+  state = { 
+    name: restaurants.retaurantName,
+    country: restaurants.country   
+  }
+
   render() {
     return (
       <div className="flex">
--- src/Components/NeighborhoodMap.js
+++ src/Components/NeighborhoodMap.js
@@ -6,16 +6,20 @@ import { faUtensils } from '@fortawesome/free-solid-svg-icons'
 const restaurants = require('./restaurants');
 
 class NeighborhoodMap extends Component {
+  restaurants = this.state;
+  
   static defaultProps = {
     center: {
       lat: 29.75,
       lng: -95.35
     },
-    zoom: 11,
+    zoom: 12,
   };
 
   render() {
-    const Marker = ({ text }) => <div><FontAwesomeIcon icon={faUtensils} />{text}</div>;
+    const Markers = (props) => {
+      return <div><FontAwesomeIcon icon={faUtensils} size="2x" color="#E57200" /><h1>{}</h1></div>
+    };
 
     return (
       <div className="flex map" style={{ height: '100vh', width: '100%'}}>
@@ -24,11 +28,13 @@ class NeighborhoodMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <Marker
-            lat={29.75}
-            lng={-95.35}
-            text={restaurants[0].retaurantName}
-          />
+          {restaurants.map(restaurant => (
+            <Markers key={restaurant.key}
+              name={restaurant.restaurantName}
+              lat={restaurant.lat}
+              lng={restaurant.lng}
+            />
+          ))}
         </GoogleMapReact>
       </div>
     )
@@ -37,4 +43,12 @@ class NeighborhoodMap extends Component {
 
 export default NeighborhoodMap;
 
-// AIzaSyCEoezQTKEDQQzCOv71vNzfu5JrVjomoyo
+// AIzaSyCEoezQTKEDQQzCOv71vNzfu5JrVjomoyo  
+
+//const Marker = ({ text }) => <div><FontAwesomeIcon icon={faUtensils} />{text}</div>;
+
+/*<Marker
+            lat={29.75}
+            lng={-95.35}
+            text={restaurants[0].retaurantName}
+          />*/
